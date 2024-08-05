c322295d30dd9b601714b8bd8e4d4654b4e750e9
Vinod <vins13pattar@live.com>
2024-08-05 21:10:08 +0530

    Static page is done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,45 @@
-const productElement = document.getElementById('product');
+const products = [
+    {
+        id: 1,
+        name: 'Ice Cream 1',
+        price: 10.99,
+        description: 'This is Ice Cream 1',
+        image: 'https://placehold.co/350x350/blue/white?text=Ice+cream+1'
+    },
+    {
+        id: 2,
+        name: 'Ice Cream 2',
+        price: 19.99,
+        description: 'This is Ice Cream 2',
+        image: 'https://placehold.co/350x350/blue/white?text=Ice+cream+2'
+    },
+    {
+        id: 3,
+        name: 'Ice Cream 3',
+        price: 14.99,
+        description: 'This is Ice Cream 3',
+        image: 'https://placehold.co/350x350/blue/white?text=Ice+cream+3'
+    },
+    {
+        id: 4,
+        name: 'Ice Cream 4',
+        price: 10.99,
+        description: 'This is Ice Cream 4',
+        image: 'https://placehold.co/350x350/blue/white?text=Ice+cream+4'
+    },
+    {
+        id: 5,
+        name: 'Ice Cream 5',
+        price: 19.99,
+        description: 'This is Ice Cream 2',
+        image: 'https://placehold.co/350x350/blue/white?text=Ice+cream+5'
+    },
+    {
+        id: 6,
+        name: 'Ice Cream 6',
+        price: 14.99,
+        description: 'This is Ice Cream 3',
+        image: 'https://placehold.co/350x350/blue/white?text=Ice+cream+6'
+    }
+]
+
